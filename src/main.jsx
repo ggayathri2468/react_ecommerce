34df7bdb2705989efdc6c65f394718f61216921b
ggayathri2468 <gayathripriya.sp@gmail.com>
2025-06-25 20:10:44 +0530

    add basename for route

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react_ecommerce">
   <StrictMode>
     <App />
   </StrictMode>
